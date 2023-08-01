111b52068be04e0d42459e198f211aa586e46aaa
Teejay Tamani <ttamani@multisyscorp.com>
2023-08-01 21:56:19 +0800

    add custom field support on homepage carousel

--- template-parts/content-homepage_carousel.php
+++ template-parts/content-homepage_carousel.php
@@ -6,7 +6,7 @@
         <div class="slide-content">
             <h4 class="optinal-h4"><?= the_title(); ?></h4>
             <h3><?= the_content(); ?></h3>
-            <a href="#" class="btn btn-primary">Meet our Team</a>
+            <a href="<?= the_field('button_url') ?>" class="btn btn-primary"><?= the_field('button_label') ?></a>
         </div>
     </div>
 </div>
