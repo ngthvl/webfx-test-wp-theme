e7b409becf7cda93ed560a9874d28c4ca229acaa
Teejay Tamani <ttamani@multisyscorp.com>
2023-08-01 22:00:19 +0800

    final update on index layout

--- footer.php
+++ footer.php
@@ -11,21 +11,96 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'webfx-test-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'webfx-test-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'webfx-test-theme' ), 'webfx-test-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<!-- Footer Start -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-logo">
+                    <a href="#">
+                        <?= the_custom_logo() ?>
+                    </a>
+                </div>
+                <div class="footer-social">
+                    <ul>
+                        <li>
+                            <a href="#" class="icon-facebook"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-instagram"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-youtube"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-tiktoc"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-contact">
+                    <h4>CONTACT INFORMATION</h4>
+                    <p>
+                        <?= get_theme_mod('webfx_test_company_address') ?> <br />
+                        <a class="learn-more" href="#">Get Directions <span class="icon-arrow-right"></span></a>
+                    </p>
+                    <p>
+                        Phone: <a class="color-body" href="tel:<?= get_theme_mod('webfx_test_header_contact_number')?>"><?= get_theme_mod('webfx_test_header_contact_number')?></a>
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-contact">
+                    <h4>HOURS OF OPERATION</h4>
+                    <p>
+                        <?= get_theme_mod('webfx_test_hours_of_operation') ?>
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-contact">
+                    <h4>AWARDS & ASSOCIATIONS</h4>
+                    <ul class="award-logos">
+                        <li>
+                            <img src="<?= get_theme_mod('webfx_test_company_award_1') ?>" alt="Award" />
+                        </li>
+                        <li>
+                            <img src="<?= get_theme_mod('webfx_test_company_award_2') ?>" alt="Award" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="bottom-wrap">
+                <div class="footer-left">
+                    <ul class="footer-menu">
+                        <li>
+                            <a href="#">Site Credits</a>
+                        </li>
+                        <li>
+                            <a href="#">Sitemap</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                    <div class="copyright">
+                        Copyright © 2020. All Rights Reserved
+                    </div>
+                </div>
+                <div class="back-top">
+                    <a class="learn-more yellow-link" href="#">Back to Top <span class="icon-arrow-up"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
